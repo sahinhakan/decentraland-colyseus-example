d82fd446c4d493f06151efe1d83bfbfa92965605
Hakan Şahin <hakan.sahin2@softtech.com.tr>
2022-07-29 12:22:23 +0300

    colyseus myroom logic

--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -1,25 +1,51 @@
 import { Room, Client } from "colyseus";
-import { MyRoomState } from "./schema/MyRoomState";
+import { MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
 
   onCreate (options: any) {
     this.setState(new MyRoomState());
 
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
-    });
+    this.onMessage('pickColor', (client, message) => {
+      const player = this.state.players.get(client.sessionId)
+      player.color = message.color
+      this.broadcast("flashColor", {color: message.color})
+      //this.send(client, "flashColor", {color: message.color})
+      console.log(player.name, ' picked color ', message.color)
+    })
+
+    this.onMessage('setColor', (client, message) => {
+      const player = this.state.players.get(client.sessionId) 
+      this.state.cubes.forEach((cube)=>{
+        if(cube.id == message.id){
+          cube.color = player.color
+        }
+      })
+      console.log(player.name, ' changed ', message.id, " to ", player.color)
+    })
 
   }
 
   onJoin (client: Client, options: any) {
-    console.log(client.sessionId, "joined!");
+ 
+    const newPlayer = new Player(
+      client.id,
+      options.userData.displayName || 'Anonymous'
+    )
+    this.state.players.set(client.sessionId, newPlayer)
+    console.log(newPlayer.name, 'joined! => ', options.userData)
+    console.log('Client: ', client)
+    console.log('-------------------------------------------------')
+
   }
 
   onLeave (client: Client, consented: boolean) {
-    console.log(client.sessionId, "left!");
+    const player = this.state.players.get(client.sessionId)
+    console.log(player.name, 'left!')
+    console.log(client.sessionId, "left!")
+
+    this.state.players.delete(client.sessionId)
+    //eğer delete etmezsek de odada kimse kalmayınca dispose olur mu?
   }
 
   onDispose() {
