f6dea53fb0db10090da513ac647cf63d977c8588
Hakan Şahin <hakan.sahin2@softtech.com.tr>
2022-07-29 11:38:14 +0300

    colyseus server init

--- src/scenes/scene1.ts
+++ src/scenes/scene1.ts
@@ -6,35 +6,39 @@ export function CreateScene1(): void {
 	//sahnedeki tüm objeleri sürekli olarak döndüren sistemi ekledik
 	//new RotatorSystem();
 
-	/// --- Spawn a cube ---
-	/* const cube = new Cube(8,1,8)
-
-	cube.addComponent(
-		new OnPointerDown(() => {
-			//uzunlugunu artırıp genişliğini küçültüyor.
-			//cube.getComponent(Transform).scale.z *= 1.1
-			//cube.getComponent(Transform).scale.x *= 0.9
-	
-			new Cube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
-		})
-	) */
-
-
 	//Spawn a mouse
 	const mouse = new Mouse({
 		position: new Vector3(6,0,6),
 		scale: new Vector3(3,3,3)
 	})
+	mouse.addComponent(new LerpData())
 
 	//follow sistemini ekliyorum
 	engine.addSystem(new LerpMove())
 
-	mouse.addComponent(new LerpData())
-
+	//Spawn another mouse
 	const mouseWill = new Mouse({
 		position: new Vector3(2,0,2),
 		scale: new Vector3(2,2,2)
 	}, resources.models.mouseWill)
-	
 	mouseWill.addComponent(new LerpData(7500))
+
+  //create messagebus
+  const sceneMessageBus = new MessageBus()
+  sceneMessageBus.on('createCube', (e) => {
+    new Cube(e.x * 8 + 1, e.y * 8, e.z * 8 + 1)
+  })
+
+	/// --- Spawn a cube ---
+	const cube = new Cube(8,1,8)
+
+	cube.addComponent(
+		new OnPointerDown(() => {
+			const x : number = Math.random()
+			const y : number = Math.random()
+			const z : number = Math.random()
+      sceneMessageBus.emit('createCube', {x, y, z})
+			//new Cube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+		})
+	)
 }
